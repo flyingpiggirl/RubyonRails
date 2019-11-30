4bf1d2c95775832f12dedc5355f605b3ac9c67d1
flyingpiggirl <12adesidaaal@gmail.com>
2019-11-30 14:25:24 +0000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'pages/info'
-  root 'ideas#index')
+  root 'ideas#index'
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
